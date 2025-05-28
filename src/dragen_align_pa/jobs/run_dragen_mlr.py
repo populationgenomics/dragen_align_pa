88143d73945654dbb8f25fa678659f1571d7e1b6
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-05-28 15:16:02 +1200

    check that this gives the analysis id

--- src/dragen_align_pa/jobs/run_dragen_mlr.py
+++ src/dragen_align_pa/jobs/run_dragen_mlr.py
@@ -64,10 +64,7 @@ def run_mlr(
                     --input-gvcf-file-url ${{gvcf}} \
                     --analysis-instance-tier {config_retrieve(['ica', 'mlr', 'analysis_instance_tier'])}
 
-                    ls {sg_name}
-
-                    cat {sg_name}/analysis.sample-{sg_name}-run-*.json
-                    cat {sg_name}/analysis.sample-{sg_name}-run-*.json | jq -r ".ica_analysis_details.id"
+                    cat {sg_name}/sample-{sg_name}-run-{sg_name}-mlr.json | jq -r ".ica_analysis_details.id"
                 """  # noqa: E501
     )
 
