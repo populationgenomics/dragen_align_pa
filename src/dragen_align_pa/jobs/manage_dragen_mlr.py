fc00f236b95cd2490db3738fd8699c6a8f287d3a
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-06-27 10:17:50 +1200

    address review comments

--- src/dragen_align_pa/jobs/manage_dragen_mlr.py
+++ src/dragen_align_pa/jobs/manage_dragen_mlr.py
@@ -159,8 +159,24 @@ def _run(  # noqa: PLR0915
             ):
                 continue
 
+            # Get an MLR analysis ID for each sequencing group, if it exists.
+            mlr_analysis_id: str = ''
+            if mlr_pipeline_id_file_exists := mlr_pipeline_id_file.exists():
+                with mlr_pipeline_id_file.open('r') as mlr_pid_fhandle:
+                    mlr_analysis_id = json.load(mlr_pid_fhandle)['pipeline_id']
+
+            # Handle cancelling a pipeline first
+            # Cancel a running job in ICA
+            if config_retrieve(key=['ica', 'management', 'cancel_cohort_run'], default=False) and mlr_analysis_id:
+                logger.info(f'Cancelling pipeline run: {mlr_analysis_id} for sequencing group {sg_name}')
+                cancel_ica_pipeline_run.run(ica_pipeline_id=mlr_analysis_id, api_root=api_root, is_mlr=True)
+                delete_pipeline_id_file(pipeline_id_file=str(mlr_pipeline_id_file))
+            else:
+                # Can't cancel a run if we don't have a pipeline ID file
+                continue
+
             # If a pipeline ID file doesn't exist we have to submit a new run, regardless of other settings
-            if not mlr_pipeline_id_file.exists():
+            if not mlr_pipeline_id_file_exists:
                 mlr_analysis_id: str = _submit_mlr_run(
                     pipeline_id_arguid_path=pipeline_id_arguid_path_dict[f'{sg_name}_pipeline_id_and_arguid'],
                     bucket=bucket,
@@ -175,15 +191,6 @@ def _run(  # noqa: PLR0915
                 )
                 with mlr_pipeline_id_file.open('w') as mlr_fhandle:
                     mlr_fhandle.write(json.dumps({'pipeline_id': mlr_analysis_id, 'ar_guid': ar_guid}))
-            else:
-                # Get an existing pipeline ID
-                with mlr_pipeline_id_file.open('r') as mlr_pid_fhandle:
-                    mlr_analysis_id = json.load(mlr_pid_fhandle)['pipeline_id']
-                # Cancel a running job in ICA
-                if config_retrieve(key=['ica', 'management', 'cancel_cohort_run'], default=False):
-                    logger.info(f'Cancelling pipeline run: {mlr_analysis_id} for sequencing group {sg_name}')
-                    cancel_ica_pipeline_run.run(ica_pipeline_id=mlr_analysis_id, api_root=api_root, is_mlr=True)
-                    delete_pipeline_id_file(pipeline_id_file=str(mlr_pipeline_id_file))
 
             mlr_pipeline_status: str = monitor_dragen_pipeline.run(
                 ica_pipeline_id=mlr_analysis_id, api_root=api_root, is_mlr=True
@@ -219,28 +226,24 @@ def _run(  # noqa: PLR0915
                     f'The pipeline {mlr_analysis_id} has failed, deleting pipeline ID file {sg_name}_pipeline_id'
                 )
 
-            # ICA has a max concurrent running pipeline limit of 20, but I have observed it as low as 16 before.
-            # Once we reach this number of running pipelines, we don't need to check on the status of others.
-            if len(running_pipelines) >= 16:  # noqa: PLR2004
-                continue
-        # If some pipelines have been cancelled, abort this pipeline
-        # This code will only trigger if a 'cancel pipeline' run is submitted before this master pipeline run is
-        # cancelled in Hail Batch
-        if cancelled_pipelines:
-            # If there are both cancelled and failed pipelines,
-            # write the failed pipelines to the error log before exiting.
-            if failed_pipelines:
-                with open('tmp_errors.log') as tmp_log_handle:
-                    lines: list[str] = tmp_log_handle.readlines()
-                    with outputs[f'{cohort.name}_errors'].open('a') as gcp_error_log_file:
-                        gcp_error_log_file.write('\n'.join(lines))
-            raise Exception(f'The following MLR pipelines have been cancelled: {" ".join(cancelled_pipelines)}')
+            # If some pipelines have been cancelled, abort this pipeline
+            # This code will only trigger if a 'cancel pipeline' run is submitted before this master pipeline run is
+            # cancelled in Hail Batch
+            if cancelled_pipelines:
+                # If there are both cancelled and failed pipelines,
+                # write the failed pipelines to the error log before exiting.
+                if failed_pipelines:
+                    with open('tmp_errors.log') as tmp_log_handle:
+                        lines: list[str] = tmp_log_handle.readlines()
+                        with outputs[f'{cohort.name}_errors'].open('a') as gcp_error_log_file:
+                            gcp_error_log_file.write('\n'.join(lines))
+                raise Exception(f'The following MLR pipelines have been cancelled: {" ".join(cancelled_pipelines)}')
 
-        # If more than 5% of pipelines are failing, exit now so we can investigate
-        if failed_pipelines and float(len(failed_pipelines)) / float(len(cohort.get_sequencing_groups())) > 0.05:  # noqa: PLR2004
-            raise Exception(
-                f'More than 5% of MLR pipelines have failed. Failing pipelines: {" ".join(failed_pipelines)}'
-            )
+            # If more than 5% of pipelines are failing, exit now so we can investigate
+            if failed_pipelines and float(len(failed_pipelines)) / float(len(cohort.get_sequencing_groups())) > 0.05:  # noqa: PLR2004
+                raise Exception(
+                    f'More than 5% of MLR pipelines have failed. Failing pipelines: {" ".join(failed_pipelines)}'
+                )
         # Catch just in case everything is finished on the first pass through the loop
         if (len(completed_pipelines) + len(cancelled_pipelines) + len(failed_pipelines)) == len(
             cohort.get_sequencing_groups()
