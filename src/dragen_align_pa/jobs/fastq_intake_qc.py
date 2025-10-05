fe5af6c4adf760ab0fcc1413f0ee912d0ab8cb61
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-10-06 12:39:52 +1300

    hopefully run md5 pipeline

--- src/dragen_align_pa/jobs/fastq_intake_qc.py
+++ src/dragen_align_pa/jobs/fastq_intake_qc.py
@@ -10,6 +10,7 @@ from hailtop.batch.job import PythonJob
 from icasdk.apis.tags import project_data_api
 from loguru import logger
 
+from dragen_align_pa.jobs import run_intake_qc_pipeline
 from dragen_align_pa.utils import get_ica_secrets
 
 
@@ -22,19 +23,37 @@ def _initalise_md5_job(cohort: Cohort) -> PythonJob:
     return md5_job
 
 
+def _get_fastq_ica_id_list(
+    fastq_filenames: list[str], configuration: icasdk.Configuration, path_parameters: dict[str, str]
+) -> list[str]:
+    with icasdk.ApiClient(configuration=configuration) as api_client:
+        api_instance = project_data_api.ProjectDataApi(api_client)
+        fastq_ids: list[str] = []
+        api_response = api_instance.get_project_data_list(  # pyright: ignore[reportUnknownVariableType]
+            path_params=path_parameters,  # pyright: ignore[reportArgumentType]
+            query_params={'filename': fastq_filenames, 'filenameMatchMode': 'EXACT'},
+        )  # type: ignore
+        for item in list(range(len(api_response.body['items']))):
+            fastq_ids.append(api_response.body['items'][item]['data']['id'])  # pyright: ignore[reportUnknownArgumentType]
+
+    return fastq_ids
+
+
 def run_md5_job(cohort: Cohort, outputs: cpg_utils.Path, api_root: str) -> PythonJob:
     job: PythonJob = _initalise_md5_job(cohort=cohort)
-    fastq_ids: list[str] = _run(cohort=cohort, api_root=api_root)
+    fastq_ids: str = _run(cohort=cohort, api_root=api_root)
     with outputs.open('w') as output_fh:
         output_fh.write('\n'.join(fastq_ids))
     logger.info(fastq_ids)
     return job
 
 
-def _run(cohort: Cohort, api_root: str) -> list[str]:
+def _run(cohort: Cohort, api_root: str) -> str:
     manifest_file_path: cpg_utils.Path = config_retrieve(['workflow', 'manifest_gcp_path'])
     with cpg_utils.to_path(manifest_file_path).open() as manifest_fh:
         supplied_checksum_data: pd.DataFrame = pd.read_csv(manifest_fh, usecols=['Filenames', 'Checksum'])
+
+    fastq_filenames: list[str] = supplied_checksum_data['Filenames'].to_list()
 
     secrets: dict[Literal['projectID', 'apiKey'], str] = get_ica_secrets()
     project_id: str = secrets['projectID']
@@ -44,14 +63,13 @@ def _run(cohort: Cohort, api_root: str) -> list[str]:
     configuration.api_key['ApiKeyAuth'] = api_key
     path_parameters: dict[str, str] = {'projectId': project_id}
 
-    with icasdk.ApiClient(configuration=configuration) as api_client:
-        api_instance = project_data_api.ProjectDataApi(api_client)
-        fastq_ids: list[str] = []
-        api_response = api_instance.get_project_data_list(
-            path_params=path_parameters,
-            query_params={'filename': supplied_checksum_data['Filenames'].to_list(), 'filenameMatchMode': 'EXACT'},
-        )
-        for item in list(range(len(api_response.body['items']))):
-            fastq_ids.append(api_response.body['items'][item]['data']['id'])
+    # Get all ica file ids for the fastq files by matching exactly on filename
+    fastq_ids: list[str] = _get_fastq_ica_id_list(
+        fastq_filenames=fastq_filenames, configuration=configuration, path_parameters=path_parameters
+    )
 
-    return fastq_ids
+    # Trigger md5sum pipeline in ICA
+    md5_pipeline_id: str = run_intake_qc_pipeline.run_md5_pipeline(
+        ica_fastq_ids=fastq_ids, api_config=configuration, project_id=project_id
+    )
+    return md5_pipeline_id
---
+++ src/dragen_align_pa/jobs/run_intake_qc_pipeline.py
@@ -0,0 +1,37 @@
+from typing import Any
+
+import icasdk
+from cpg_utils.config import config_retrieve
+from icasdk.apis.tags import project_analysis_api
+from icasdk.model.analysis_data_input import AnalysisDataInput
+from icasdk.model.analysis_tag import AnalysisTag
+from icasdk.model.create_nextflow_analysis import CreateNextflowAnalysis
+from icasdk.model.nextflow_analysis_input import NextflowAnalysisInput
+
+
+def run_md5_pipeline(ica_fastq_ids: list[str], api_config: icasdk.Configuration, project_id: str) -> str:
+    header_params: dict[Any, Any] = {}
+    body = CreateNextflowAnalysis(
+        userReference='',
+        pipelineId=config_retrieve(['ica', 'pipelines', 'md5_pipeline_id']),
+        tags=AnalysisTag(
+            technicalTags=[*config_retrieve(['ica', 'tags', 'technical_tags']), 'md5sum'],
+            userTags=config_retrieve(['ica', 'tags', 'user_tags']),
+            referenceTags=config_retrieve(['ica', 'tags', 'reference_tags']),
+        ),
+        outputParentFolderId='',
+        analysisInput=NextflowAnalysisInput(inputs=[AnalysisDataInput(parameterCode='in', dataIds=ica_fastq_ids)]),
+    )
+
+    with icasdk.ApiClient(configuration=api_config) as api_client:
+        api_instance = project_analysis_api.ProjectAnalysisApi(api_client)
+        path_params: dict[str, str] = {'projectId': project_id}
+        try:
+            api_response = api_instance.create_nextflow_analysis(  # pyright: ignore[reportUnknownVariableType]
+                path_params=path_params, header_params=header_params, body=body
+            )
+            return api_response.body['id']  # pyright: ignore[reportUnknownVariableType]
+        except icasdk.ApiException as e:
+            raise icasdk.ApiException(
+                f'Exception when calling ProjectAnalysisApi->create_nextflow_analysis: {e}'
+            ) from e
