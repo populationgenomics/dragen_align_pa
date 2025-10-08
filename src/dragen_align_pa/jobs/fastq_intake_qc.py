938f9010bd4e21a5f4d7b33acb379465c90f8acf
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-10-08 15:43:47 +1300

    whats happening

--- src/dragen_align_pa/jobs/fastq_intake_qc.py
+++ src/dragen_align_pa/jobs/fastq_intake_qc.py
@@ -67,10 +67,9 @@ def _get_md5_pipeline_outputs(
 ) -> str:
     # Get the ID
     with md5_pipeline_file.open('r') as pipeline_fh:
-        for line in pipeline_fh:
-            print(line)
-        pipeline_id: str = json.load(pipeline_fh)['pipeline_id']
-        ar_guid: str = json.load(pipeline_fh)['ar_guid']
+        pipeline_data: dict[str, str] = json.load(pipeline_fh)
+        pipeline_id: str = pipeline_data['pipeline_id']
+        ar_guid: str = pipeline_data['ar_guid']
     api_response = api_instance.get_project_data_list(  # pyright: ignore[reportUnknownVariableType]
         path_params=path_parameters,  # pyright: ignore[reportArgumentType]
         query_params={
