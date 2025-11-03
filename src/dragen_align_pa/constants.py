679a7c8b360290619d1e139887a7ad005ee5ce1a
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-11-04 09:58:34 +1300

    check that read type is set in config

--- src/dragen_align_pa/constants.py
+++ src/dragen_align_pa/constants.py
@@ -3,7 +3,10 @@ from typing import Final
 import cpg_utils
 from cpg_utils.config import config_retrieve, output_path
 
-READS_TYPE: Final = config_retrieve(['workflow', 'reads_type']).lower()
+READS_TYPE: Final = config_retrieve(['workflow', 'reads_type'], default=None).lower()
+assert READS_TYPE in {'fastq', 'cram'}, (
+    f'Unsupported reads type: {READS_TYPE}. Valid options are fastq or cram. Please set this in the configuration using [workflow][reads_type].'  # noqa: E501
+)
 BUCKET: Final = cpg_utils.to_path(output_path(suffix=''))
 BUCKET_NAME: Final = str(BUCKET).removeprefix('gs://').removesuffix('/')
 DRAGEN_VERSION: Final = config_retrieve(['ica', 'pipelines', 'dragen_version'])
