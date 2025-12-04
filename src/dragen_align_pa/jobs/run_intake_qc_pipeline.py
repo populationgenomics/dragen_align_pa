2ffa1fb02484627864a1215fe93e1080ad5e5919
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-12-04 14:32:32 +1300

    try a fix

--- src/dragen_align_pa/jobs/run_intake_qc_pipeline.py
+++ src/dragen_align_pa/jobs/run_intake_qc_pipeline.py
@@ -37,9 +37,9 @@ def run_md5_pipeline(
                 AnalysisDataInput(parameterCode='in', dataIds=[fastq_list_file_id]),
             ],
             parameters=[
-                {'parameterCode': 'ica_project_id', 'values': project_id},
-                {'parameterCode': 'ica_api_key', 'values': api_key},
-                {'parameterCode': 'chunk_size', 'values': chunk_size},
+                {'parameterCode': 'ica_project_id', 'values': [project_id]},
+                {'parameterCode': 'ica_api_key', 'values': [api_key]},
+                {'parameterCode': 'chunk_size', 'values': [chunk_size]},
             ],
         ),
         analysisStorageId=config_retrieve(['ica', 'pipelines', 'analysis_storage_id']),
