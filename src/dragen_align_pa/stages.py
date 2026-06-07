379d9dfc378d2227b9b11189a9642fd5d1ef915e
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2026-06-08 10:04:47 +1200

    more logging

--- src/dragen_align_pa/stages.py
+++ src/dragen_align_pa/stages.py
@@ -231,7 +231,7 @@ class UploadDataToIca(SequencingGroupStage):
             job.image(image=get_driver_image())
             # Debugging logging
             logger.info(f'Sequencing group: {sequencing_group}')
-            logger.info(f'Path for sequencing group: {sequencing_group} is {sequencing_group.cram.path}')
+            # logger.info(f'Path for sequencing group: {sequencing_group} is {sequencing_group.cram.path}')
             # job.storage(calculate_needed_storage(cram_path=sequencing_group.cram.path))
             job.memory('8Gi')
             job.spot(is_spot=False)
